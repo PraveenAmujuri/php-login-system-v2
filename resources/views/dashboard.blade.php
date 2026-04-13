0961604b2d9501b2a18142def671390d85fd0091
PraveenAmujuri <saipraveenamujuri@gmail.com>
2026-04-13 15:41:20 +0530

    feat: display user details and audit logs on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,11 +2,17 @@
 <html>
 <body>
 
-<h2>Dashboard</h2>
+<h3>Welcome {{ $user->userId }}</h3>
 
-<p>You are logged in</p>
+<p>Last Login: {{ $user->last_login }}</p>
 
+<h4>Your Activity</h4>
+
+<ul>
+@foreach($logs as $log)
+    <li>{{ $log->action }} at {{ $log->created_at }}</li>
+@endforeach
+</ul>
 <a href="/logout">Logout</a>
-
 </body>
 </html>
